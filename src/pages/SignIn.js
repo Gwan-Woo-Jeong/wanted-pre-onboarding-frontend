6676a4f254401a6bb801ac17a2590a69d5349715
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-08-11 20:08:49 +0900

    feat: initial project setup & install required libraries

---
+++ src/pages/SignIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignIn() {
+  return <div id="sign-in">SignIn</div>;
+}
+
+export default SignIn;
---
+++ src/pages/Todo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Todo() {
+  return <div id="todo">Todo</div>;
+}
+
+export default Todo;
