fe001caf4497f14f87ebfe0082dc671ab169e5db
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-08-13 00:28:21 +0900

    fix: SVG attribute names from kebab-case to camelCase

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -53,14 +53,14 @@ function SignUp({ onSignupSuccess }) {
           <svg
             fill="none"
             stroke="currentColor"
-            stroke-width="1.1"
+            strokeWidth="1.1"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
             aria-hidden="true"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15.75 19.5L8.25 12l7.5-7.5"
             ></path>
           </svg>
